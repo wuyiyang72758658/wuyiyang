e7efc9fa4007e599cf49d8b7b3ac3a3b7d06ac09
wuyiyang <1437385650@qq.com>
2022-03-13 19:52:28 +0800

    修改了新文件

--- hello-app/src/com/jin/hello/Hello.java
+++ hello-app/src/com/jin/hello/Hello.java
@@ -3,5 +3,6 @@ package com.jin.hello;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("今天是个好日子");
+        System.out.println("十多分v为u的规划v货物热改变 ");
     }
 }
